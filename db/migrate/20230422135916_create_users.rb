e27791564cd9fbaecbd1faa1b580ddab89d3175a
Ramez Al-Siddiq <ramez.sydney@gmail.com>
2023-04-23 00:15:00 +1000

    added comments to make things readable and edited migration as required

--- db/migrate/20230422135916_create_users.rb
+++ db/migrate/20230422135916_create_users.rb
@@ -1,10 +1,16 @@
+# Generated a User Model through the terminal and created a table called 'users' with several columns.
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
+      # Adding a username column with a text data type
       t.text :username
+       # Adding an email column with a text data type
       t.text :email
+       # Adding a password_digest column with a string data type
       t.string :password_digest
+       # Adding a favorite genres column with a text data type
       t.text :favorite_genres
+       # Adding a data joined column with a datetime data type
       t.datetime :date_joined
 
       t.timestamps
