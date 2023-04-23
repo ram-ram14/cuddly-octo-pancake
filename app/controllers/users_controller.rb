f77a00b7880da0528a6ac728610fbc3991ef45b5
Ramez Al-Siddiq <ramez.sydney@gmail.com>
2023-04-23 19:18:00 +1000

    added methods to the controller for each action in the users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,52 @@
 class UsersController < ApplicationController
+
+  before_action :check_for_admin, :only => [:index]
+
   def index
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path
+  end
+
+end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :favorite_genres, :date_joined)
   end
 end
