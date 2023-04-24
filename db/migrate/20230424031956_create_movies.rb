306fc4ad653770b56cf2e29fdb73f33771318f2f
Ramez Al-Siddiq <ramez.sydney@gmail.com>
2023-04-24 13:25:26 +1000

    generated a movie model and edited the migration file

---
+++ db/migrate/20230424031956_create_movies.rb
@@ -0,0 +1,15 @@
+class CreateMovies < ActiveRecord::Migration[5.2]
+  def change
+    create_table :movies do |t|
+      t.text :title
+      t.text :plot
+      t.text :director
+      t.datetime :release_date
+      t.text :runtime
+      t.text :poster_image
+      t.text :cast
+
+      t.timestamps
+    end
+  end
+end
