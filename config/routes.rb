0c5fc36f231b82dea7318c91669583e9529f5f4f
Ramez Al-Siddiq <ramez.sydney@gmail.com>
2023-04-28 10:31:16 +1000

    added more comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,22 @@
 Rails.application.routes.draw do
-  # Movies routes
+  # Movies routes (added the routes for the movies resource)
   resources :movies do
-    resources :reviews, only: [:new, :create, :index]
-    get 'add_review', on: :member
+  resources :reviews, only: [:new, :create, :index]
+  get 'add_review', on: :member
   end
 
-  # Users routes
+  # Users routes (added routes for users resource)
   resources :users, :only => [:index, :new, :create]
 
-
-  # Sessions routes
+  # Sessions routes (added routes for sessions)
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   delete '/login' => 'sessions#destroy'
 
-  # Registration routes
+  # Registration routes (added routes for registration)
   get '/register' => 'users#new'
   post '/register' => 'users#create'
 
-  # Home route
+  # Home route (added a route for the application)
   root 'movies#index'
 end
